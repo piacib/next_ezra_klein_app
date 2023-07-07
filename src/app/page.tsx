b3ef49fc0a83a53e9d3ba687653532e5e96522b8
piacib <ben.piacitelli@gmail.com>
2023-07-07 12:56:23 -0700

    added test component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-
+"use client";
+import Header from "@/components/Header/Header";
+import styled from "styled-components";
+const Container = styled.div`
+  color: green;
+`;
 export default function Home() {
-  return <></>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
