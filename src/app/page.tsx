228f97ae35605841847817f06e4b3c7f84443fe0
piacib <ben.piacitelli@gmail.com>
2023-07-08 12:22:21 -0700

    set up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
-import Header from "@/components/Header/Header";
 import StyleWrapper from "@/styles/StyleWrapper";
+import { mostPopular, recommendedId } from "@/data_temp";
+import BookCarousel from "@/components/BookCarousel/BookCarousel";
+import Categories from "@/components/Categories/Categories";
+const categories = [
+  "Fiction / Fantasy / Historical",
+  "Fiction / Fantasy / Epic",
+  "Fiction / Literary",
+  "Fiction / Alternative History",
+  "Fiction / Fantasy / Dark Fantasy",
+  "Fiction / Fairy Tales, Folk Tales, Legends & Mythology",
+];
 export default function Home() {
+  const mostPopularIds = mostPopular();
+  const recomended = recommendedId();
   return (
     <StyleWrapper>
-      <Header />
+      <BookCarousel title={"Popular"} books={mostPopularIds} />
+      <BookCarousel title={"Recommended"} books={recomended} />
+      <Categories list={categories} />
     </StyleWrapper>
   );
 }
