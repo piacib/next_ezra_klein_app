7276f1faf28d02164d95c07661802f0f1ffbd677
piacib <ben.piacitelli@gmail.com>
2023-07-10 10:57:34 -0700

    added api endpoint

---
+++ src/app/api/data.ts
@@ -0,0 +1,8 @@
+import alldata from "@/data/allData.json";
+import { NextApiRequest, NextApiResponse } from "next";
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): void {
+  res.status(200).json(alldata);
+}
---
+++ src/app/api/test.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): void {
+  res.status(200).json({ test: "hello" });
+}
