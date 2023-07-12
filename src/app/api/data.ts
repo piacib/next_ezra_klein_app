83ef5c7f941b7081f5be7d4993f863bb2d144629
piacib <ben.piacitelli@gmail.com>
2023-07-11 17:51:49 -0700

    removed

--- src/app/api/data.ts
+++
@@ -1,8 +0,0 @@
-import alldata from "@/data/allData.json";
-import { NextApiRequest, NextApiResponse } from "next";
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): void {
-  res.status(200).json(alldata);
-}
